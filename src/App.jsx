6d6319b43c4f75f411e884d1219099f010b98f38
olufiks <fikayoamos7@gmail.com>
2026-07-27 09:50:15 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <header>
+      <h1>olufikayo | portfolio</h1>
+      <h2>about</h2>
+      <h2>skills</h2>
+      <h2>my work</h2>
+      <h2>contact</h2>
+    </header>
+    </>
+  );
+}
+
+export default App;
